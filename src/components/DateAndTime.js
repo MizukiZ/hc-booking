89d8ecd70c831e3720babb296df6ca3877c66ed0
Mizuki Zenta <mizukisb@gmail.com>
2019-07-27 13:24:48 +1000

    added react schduler for booking with schduler UI

--- src/components/DateAndTime.js
+++ src/components/DateAndTime.js
@@ -1,12 +1,93 @@
-import React, { useState, Fragment } from "react";
-import Grid from '@material-ui/core/Grid';
+import React, { useState } from "react";
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler, WeekView, Appointments, Toolbar, DateNavigator
+} from "@devexpress/dx-react-scheduler-material-ui";
+
 
 function DateAndTime({ options }) {
+  let [currentDate, currentDateChange] = useState(new Date())
+  const originalscheduleData = [
+    // hold original schduleData
+    { startDate: '2019-7-21 10:00', endDate: '2019-7-21 12:00', title: 'Taken', available: false },
+    { startDate: '2019-7-26 10:00', endDate: '2019-7-26 12:00', title: 'Available', available: true },
+    { startDate: '2019-7-26 13:00', endDate: '2019-7-26 15:00', title: 'Available', available: true },
+    { startDate: '2019-7-26 15:00', endDate: '2019-7-26 17:00', title: 'Available', available: true }
+  ]
+  let [scheduleData, updatescheduleData] = useState([
+    // date which is generated based on the appointments and admin avilavility
+    { startDate: '2019-7-21 10:00', endDate: '2019-7-21 12:00', title: 'Taken', available: false },
+    { startDate: '2019-7-26 10:00', endDate: '2019-7-26 12:00', title: 'Available', available: true },
+    { startDate: '2019-7-26 13:00', endDate: '2019-7-26 15:00', title: 'Available', available: true },
+    { startDate: '2019-7-26 15:00', endDate: '2019-7-26 17:00', title: 'Available', available: true }
+  ])
+
+  const Appointment = ({
+    children, style, ...restProps
+  }) => {
+    let bgColor = 'linear-gradient(45deg, #2196f3 30%, #21cbf3 90%)'
+    let boxShadow = ''
+    if (!children[1].props.data.available) {
+      // if the appointment is taken, change the bg color to red
+      bgColor = 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
+    }
+    if (children[1].props.data.selected) {
+      // if selected, add boxshadow
+      boxShadow = '1px 1px 8px 1px #000000'
+    }
+    return (
+      <Appointments.Appointment
+        {...restProps}
+        style={{
+          ...style,
+          background: bgColor,
+          boxShadow: boxShadow
+        }}
+        onClick={() => {
+          if (!children[1].props.data.selected && children[1].props.data.available) {
+            // click event for appointment comp
+
+            let apData = children[1].props.data
+            // remove chosen schedule from scheduleData
+            let filterdscheduleData = originalscheduleData.filter((ap) => {
+              return JSON.stringify(ap) !== JSON.stringify(apData)
+            })
+
+            // bring back the chosen schdule with selected true attr
+            apData["selected"] = true
+            const finalizedscheduleData = [...filterdscheduleData, apData]
+            // update schedule data
+            updatescheduleData(finalizedscheduleData)
+          }
+        }}
+      >
+        {children}
+      </Appointments.Appointment>
+    )
+  };
 
   return (
-    <Fragment>
-
-    </Fragment>
+    <Scheduler
+      data={scheduleData}
+    >
+      <ViewState
+        currentDate={currentDate}
+        onCurrentDateChange={(date) => {
+          currentDateChange(date)
+        }}
+      />
+      <WeekView
+        // detemine which days to show (zero based. 0-Sunday 6-Saturday)
+        excludedDays={[4, 6]}
+        // set start time from admin setting
+        startDayHour={10}
+        // set end time from admin setting
+        endDayHour={19}
+      />
+      <Toolbar />
+      <DateNavigator />
+      <Appointments appointmentComponent={Appointment} />
+    </Scheduler>
   );
 }
 
